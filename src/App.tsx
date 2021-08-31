7425908ac9bf23333ace5318238ac2c27a5e4ddd
HamazakiHisanori <81221219+HamazakiHisanori@users.noreply.github.com>
2021-09-01 05:44:58 +0900

    TypeScript type-definition

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,36 @@
 import axios from "axios";
+import { useState } from "react";
 import "./styles.css";
 
+import { Todo } from "./Todo";
+
+type TodoType = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
 export default function App() {
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
   const onClickGetData = () => {
-    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
-      console.log(res);
-    });
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        setTodos(res.data);
+      });
   };
 
   return (
     <div className="App">
       <button onClick={onClickGetData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo
+          title={todo.title}
+          userId={todo.userId}
+          completed={todo.completed}
+        />
+      ))}
     </div>
   );
 }
